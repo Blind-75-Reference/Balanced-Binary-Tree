27568c5ec7c20f514799e563fbb0a5bfdb6c5ba9
LiquidPlummer <kyle.plummer@revature.com>
2023-03-23 14:23:38 -0400

    adding test cases

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,8 +1,45 @@
-
 class Solution {
+    /*
+    In this solution we use the recursive checkBalance() method to check the balance of the tree. If we find ourselves
+    on a null node, we return 0. Otherwise we recurse on left and right sub-trees. Consider the two values we get back,
+    if the difference is > 1 then we have found an imbalanced tree, and we return -1 to indicate this. Otherwise we
+    add 1 to whichever depth was greater, and return that number. If we ever get a -1 back we know there is an
+    imbalance somewhere below, and we propagate that -1 up the stack.
+
+    When we are done and return back into isBalanced() we will either have a -1 or the depth of the balanced tree.
+     */
     public boolean isBalanced(TreeNode root) {
-        //Write your solution here...
+        if(checkBalance(root) != -1) {
+            return true;
+        }
 
         return false;
     }
+
+    //base case - node is null
+        //return 0
+    //else recurse on both children
+        // imbalance = |l - r|
+    // when we find an imbalance > 1 we send a -1 up, if we ever get a -1 pass that back up
+    //otherwise send back the greater of the two depths, +1
+    //if we get a -1 back at the top we know the solution is false
+    private int checkBalance(TreeNode root) {
+        //base case
+        if(root == null) {
+            return 0;
+        }
+
+        //otherwise recurse on left and right
+        int l = checkBalance(root.left);
+        int r = checkBalance(root.right);
+
+        //check what we get back from recursion. Either propagate the -1, or check and return the max depth
+        if(l == -1 || r == -1 || Math.abs(l - r) > 1) {
+            return -1;
+        } else {
+            return Math.max(l, r) + 1;
+        }
+
+    }
+
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
@@ -5,6 +7,28 @@ public class TestCases {
 
     @Test
     public void TestCase1() {
+        TreeNode tree = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(3, new TreeNode(4), new TreeNode(4)),
+                        new TreeNode(3, new TreeNode(4), new TreeNode(4))),
 
+                new TreeNode(2,
+                        new TreeNode(3, new TreeNode(4), new TreeNode(4)),
+                        new TreeNode(3, new TreeNode(4), new TreeNode(4))
+                ));
+
+        Assertions.assertEquals(true, solution.isBalanced(tree));
+    }
+
+    @Test
+    public void TestCase2() {
+        TreeNode tree = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(3, new TreeNode(4), new TreeNode(4)),
+                        new TreeNode(3, new TreeNode(4), new TreeNode(4))),
+
+                new TreeNode(2));
+
+        Assertions.assertEquals(false, solution.isBalanced(tree));
     }
 }
